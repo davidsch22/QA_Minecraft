f8ec1a5e0fc81aa37653dd7dfc61e13cff3ccadc
David Schulz <deschulz22@gmail.com>
2022-04-29 10:50:11 -0500

    Merge some files from different sources

--- fileMerge.py
+++ fileMerge.py
@@ -1,4 +1,3 @@
-from distutils.file_util import write_file
 import os
 import pandas as pd
 
@@ -16,24 +15,25 @@ def main():
                     rootFolder = "GamepediaTxt"
                 files.loc[len(files.index)] = [rootFolder, dirpath, fileName]
 
+    files['FileName'] = files['FileName'].str.lower()
     duplicates = files.duplicated('FileName', keep=False)
     duplicates = files[duplicates]
 
     for fileName in duplicates['FileName'].unique():
-        file_dup = duplicates[duplicates['FileName'] == fileName]
+        file_dup = duplicates[duplicates['FileName'] == fileName].reset_index(drop=True)
         write_path = ""
         read_path = ""
         for i in range(file_dup.shape[0]):
             path = file_dup['DirPath'][i] + "/" + file_dup['FileName'][i]
-            if "Sorted" in path:
+            if write_path == "" and "Sorted" in path:
                 write_path = path
             else:
                 read_path = path
-        with os.open(write_path, 'a') as write_file:
-            with os.open(read_path, 'r') as read_file:
+        with open(write_path, 'a', encoding='utf-8') as write_file:
+            with open(read_path, 'r', encoding='utf-8') as read_file:
                 lines = read_file.readlines()
                 write_file.writelines(lines)
-        return
+        os.remove(read_path)
 
 if __name__ == "__main__":
     main()
