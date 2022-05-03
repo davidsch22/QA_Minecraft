8016f5c37f4079d2f59332ccaa6ca15bdc2ee17a
David Schulz <deschulz22@gmail.com>
2022-05-02 21:48:51 -0500

    Fix some characters in knowledge, create tf-idf table from knowledge

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
-import pandas as pd
+import os
+import math
+import string
 import nltk
+import pandas as pd
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
 
-def preprocess_info(knowledge: pd.DataFrame):
-    pass
+stop_words = set(stopwords.words('english'))
+stop_words = stop_words.union(set(string.punctuation))
+porter = PorterStemmer()
+tf_idf = pd.DataFrame(dtype='float64')
 
 
-def preprocess_questions(questions: pd.DataFrame) -> tuple[pd.Series, pd.DataFrame, pd.DataFrame]:
+def tokenize_line(line) -> list:
+    tokens = nltk.word_tokenize(line)
+    stemmed = []
+    for token in tokens:
+        if token.lower() not in stop_words:
+            stemmed.append(porter.stem(token))
+    return stemmed
+
+
+def preprocess_kd():
+    global tf_idf
+    tf = pd.DataFrame(dtype='float64')
+    idf = pd.DataFrame(dtype='float64')
+
+    for (dirPath, dirNames, fileNames) in os.walk('KnowledgeDatabase/'):
+        for fileName in fileNames:
+            filePath = dirPath + "/" + fileName
+            print(filePath[18:])
+            doc = pd.read_csv(filePath, sep='`', names=['Line'], encoding='utf-8')
+            doc = doc.applymap(lambda x: tokenize_line(x))
+            all_tokens = doc['Line'].explode()
+            total = all_tokens.shape[0]
+            unique_tokens = all_tokens.unique()
+            for token in unique_tokens:
+                count = all_tokens[all_tokens == token].shape[0]
+                tf.loc[token, filePath[18:]] = count / total
+    tf.fillna(0, inplace=True)
+    N = tf.shape[1]
+    idf = tf.apply(lambda row: math.log(N / (row[row > 0].shape[0]+1)), axis=1)
+    tf_idf = tf.mul(idf, axis=0)
+    print(tf_idf.describe())
+
+
+def preprocess_questions(questions: pd.DataFrame) -> tuple[pd.Series, pd.DataFrame]:
     totals = questions.iloc[0]
     questions = questions.drop(0).reset_index(drop=True)
     
-    q_tokens = questions.applymap(lambda x: nltk.word_tokenize(x), na_action='ignore')
-
-    q_pos = q_tokens.applymap(lambda x: nltk.pos_tag(x), na_action='ignore')
-
-    stop_words = set(stopwords.words('english'))
-    q_processed = q_tokens.applymap(lambda x: [w for w in x if not w.lower() in stop_words], na_action='ignore')
-
-    porter = PorterStemmer()
-    q_processed = q_processed.applymap(lambda x: [porter.stem(w) for w in x], na_action='ignore')
+    q_processed = questions.applymap(lambda x: tokenize_line(x), na_action='ignore')
     
-    return totals, q_pos, q_processed
+    return totals, q_processed
 
 
 def main():
-    questions = pd.read_csv('QuestionsCorpus/AllQuestions.csv', sep=';')
+    preprocess_kd()
+    
+    # questions = pd.read_csv('QuestionsCorpus/AllQuestions.csv', sep=';')
 
-    totals, q_pos, q_processed = preprocess_questions(questions)
-    print(totals)
-    print(q_pos)
-    print(q_processed)
+    # totals, q_processed = preprocess_questions(questions)
+    # print(totals)
+    # print(q_processed)
 
 
 if __name__ == "__main__":
